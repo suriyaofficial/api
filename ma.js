9136c9ca35a9fedddd991d2b01f41f0e94ba153f
suriya <suriya.r@arbaan.com>
2023-05-29 16:28:33 +0530

    js file

---
+++ ma.js
@@ -0,0 +1,11 @@
+let user = {
+    "userId": "rirani",
+    "jobTitleName": "Developer",
+    "firstName": "Romin",
+    "lastName": "Irani",
+    "preferredFullName": "Romin Irani",
+    "employeeCode": "E1",
+    "region": "CA",
+    "phoneNumber": "408-1234567",
+    "emailAddress": "romin.k.irani@gmail.comm"
+}
